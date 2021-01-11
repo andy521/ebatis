f86f7c24bb789673358d24735f875c95e6ebded6
18729021251@163.com <18729021251@163.com>
2021-01-11 11:59:44 +0800

    集成swagger的增强版本 Knife4j-2.x

--- ebatis-sample/src/main/java/io/manbang/ebatis/sample/SampleApplication.java
+++ ebatis-sample/src/main/java/io/manbang/ebatis/sample/SampleApplication.java
@@ -1,17 +1,41 @@
 package io.manbang.ebatis.sample;
 
-import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
+//import com.github.xiaoymin.swaggerbootstrapui.annotations.EnableSwaggerBootstrapUI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author 章多亮
  * @since 2020/6/1 18:25
  */
-@EnableSwaggerBootstrapUI
+//@EnableSwaggerBootstrapUI
 @SpringBootApplication
 public class SampleApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(SampleApplication.class, args);
+    static Logger logger= LoggerFactory.getLogger(SampleApplication.class);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application=SpringApplication.run(SampleApplication.class, args);
+        Environment env = application.getEnvironment();
+        String host= InetAddress.getLocalHost().getHostAddress();
+        String port=env.getProperty("server.port");
+        logger.info("\n----------------------------------------------------------\n\t" +
+                        "Application '{}' is running! Access URLs:\n\t" +
+                        "Local: \t\thttp://localhost:{}\n\t" +
+                        "External: \thttp://{}:{}\n\t"+
+                        "Doc: \thttp://{}:{}/doc.html\n\t"+
+                        "----------------------------------------------------------",
+                env.getProperty("spring.application.name"),
+                env.getProperty("server.port"),
+                host,port,
+                host,port);
+
+
+        //SpringApplication.run(SampleApplication.class, args);
     }
 }
